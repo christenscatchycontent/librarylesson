2df62b287bba76187bc1cb992a0ea13a7c8f2b58
Christen Hedrick <90578990+catchycontent@users.noreply.github.com>
2021-11-18 20:39:44 -0500

    additions

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -16,10 +16,43 @@ function sortAccountsByLastName(accounts) {
 /* It returns a sorted array of the provided account objects. The objects are sorted alphabetically by last name. */
 
 function getTotalNumberOfBorrows(account, books) {
-  const 
+  let result = 0
+  const {id} = account
+  for (let book in books) {
+    const {borrows} = books[book];
+    borrows.forEach((element) => { if (element.id === id) {
+      result++
+    }})
+  }
+  return result
+}
+/* It returns a _number_ that represents the number of times the account's ID appears in any book's `borrows` array. */
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const AuthorsById = (authors, id) => {
+    return authors.find((author) => author.id === id);
+  };
+  const accountId = account.id;
+  let result = [];
+  result = books.filter((book) => {
+    return book.borrows.some((borrow) => borrow.id === accountId && !borrow.returned);
+  });
+  result = result.map((book) => {
+    const author = AuthorsById(authors, book.authorId);
+    const newBook = {
+      ...book,
+      author,
+    };
+
+    return newBook;
+  });
+
+  return result;
 }
 
-function getBooksPossessedByAccount(account, books, authors) {}
+
+/* It returns an array of book objects, including author information, that represents all books _currently checked out_ by the given account. 
+_Look carefully at the object below,_ as it's not just the book object; the author object is nested inside of it. */
 
 module.exports = {
   findAccountById,
